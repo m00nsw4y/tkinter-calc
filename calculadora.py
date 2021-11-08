d704c3885f186d379f816fa8931aa6d067120b2e
Gabriella <89670224+gabriellavrg@users.noreply.github.com>
2021-11-08 20:07:22 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -14,7 +14,7 @@ def button_click(numero):
 def button_clear():
     e.delete(0, END)
 
-def button_add():
+def button_sum():
     numero_1 = e.get()
     global f_num 
     global math
@@ -34,7 +34,7 @@ def button_equal():
     elif math=='dividir':
         e.insert(0, f_num / int(numero_2))
 
-def button_subtrair():
+def button_sub():
     numero_1 = e.get()
     global f_num 
     global math
@@ -42,7 +42,7 @@ def button_subtrair():
     f_num = int(numero_1)
     e.delete(0, END)
 
-def button_multiplicar():
+def button_mult():
     numero_1 = e.get()
     global f_num 
     global math
@@ -50,7 +50,7 @@ def button_multiplicar():
     f_num = int(numero_1)
     e.delete(0, END)
 
-def button_dividir():
+def button_divide():
     numero_1 = e.get()
     global f_num 
     global math
@@ -59,7 +59,7 @@ def button_dividir():
     e.delete(0, END)
 
 
-#Definindo botões
+#Setting buttons
 button_1 = Button(root, text='1', padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text='2', padx=40, pady=20, command=lambda: button_click(2))
 button_3 = Button(root, text='3', padx=40, pady=20, command=lambda: button_click(3))
@@ -70,14 +70,14 @@ button_7 = Button(root, text='7', padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text='8', padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text='9', padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text='0', padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text='+', bg = '#F6B4C3', padx=39, pady=20, command=button_add)
+button_add = Button(root, text='+', bg = '#F6B4C3', padx=39, pady=20, command=button_sum)
 button_equal = Button(root, text='=', bg = '#F4F4DC', padx=91, pady=20, command=button_equal)
 button_limpar = Button(root, text='Limpar', bg = '#BECAE5', padx=79, pady=20, command= button_clear)
 button_subtrair = Button(root, text='-', bg='#F1D4C8', padx=40, pady=20, command=button_subtrair)
-button_multiplicar = Button(root, text='x', bg = '#C3BAD6', padx=41, pady=20, command=button_multiplicar)
-button_dividir = Button(root, text='/', bg='#C9DDD5', padx=40, pady=20, command=button_dividir)
+button_multiplicar = Button(root, text='x', bg = '#C3BAD6', padx=41, pady=20, command=button_mult)
+button_dividir = Button(root, text='/', bg='#C9DDD5', padx=40, pady=20, command=button_divide)
 
-#Botões na tela
+#Buttons
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
 button_3.grid(row=3, column=2)
