20cf7ba2821a53e1a3d30db39df3b06a76e010a0
Gabriella <89670224+gabriellavrg@users.noreply.github.com>
2022-05-14 23:50:49 -0300

    Update calculadora.py
    Eng translated completed.

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-root.title('Calculadora')
+root.title('Calculator')
 
 e = Entry(root, width=30, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
@@ -9,7 +9,7 @@ e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 def button_click(num):
     e.delete(0, END)
-    e.insert(0, str(e.get()) + str(numero))
+    e.insert(0, str(e.get()) + str(num))
 
 def button_clear():
     e.delete(0, END)
